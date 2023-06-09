b0f207f9d51464ec7160e3c3ed9eb3fd036770eb
Roberto Aldera <51328612+roberto-aldera@users.noreply.github.com>
2023-06-09 21:53:25 +0200

    Fix formatting

--- hello_world.c
+++ hello_world.c
@@ -4,117 +4,120 @@
 #include <stdlib.h>
 
 typedef enum {
-    EventTypeTick,
-    EventTypeKey,
+  EventTypeTick,
+  EventTypeKey,
 } EventType;
 
 typedef struct {
-    EventType type;
-    InputEvent input;
+  EventType type;
+  InputEvent input;
 } PluginEvent;
 
 typedef struct {
-    int x;
-    int y;
+  int x;
+  int y;
 } PluginState;
 
-static void render_callback(Canvas* const canvas, void* ctx) {
-    const PluginState* plugin_state = acquire_mutex((ValueMutex*)ctx, 25);
-    if(plugin_state == NULL) {
-        return;
-    }
-    // border around the edge of the screen
-    canvas_draw_frame(canvas, 0, 0, 128, 64);
+static void render_callback(Canvas *const canvas, void *ctx) {
+  const PluginState *plugin_state = acquire_mutex((ValueMutex *)ctx, 25);
+  if (plugin_state == NULL) {
+    return;
+  }
+  // border around the edge of the screen
+  canvas_draw_frame(canvas, 0, 0, 128, 64);
 
-    canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str_aligned(
-        canvas, plugin_state->x, plugin_state->y, AlignRight, AlignBottom, "Hello World!");
+  canvas_set_font(canvas, FontPrimary);
+  canvas_draw_str_aligned(canvas, plugin_state->x, plugin_state->y, AlignRight,
+                          AlignBottom, "Hello World!");
 
-    release_mutex((ValueMutex*)ctx, plugin_state);
+  release_mutex((ValueMutex *)ctx, plugin_state);
 }
 
-static void input_callback(InputEvent* input_event, FuriMessageQueue* event_queue) {
-    furi_assert(event_queue);
+static void input_callback(InputEvent *input_event,
+                           FuriMessageQueue *event_queue) {
+  furi_assert(event_queue);
 
-    PluginEvent event = {.type = EventTypeKey, .input = *input_event};
-    furi_message_queue_put(event_queue, &event, FuriWaitForever);
+  PluginEvent event = {.type = EventTypeKey, .input = *input_event};
+  furi_message_queue_put(event_queue, &event, FuriWaitForever);
 }
 
-static void hello_world_state_init(PluginState* const plugin_state) {
-    plugin_state->x = 50;
-    plugin_state->y = 30;
+static void hello_world_state_init(PluginState *const plugin_state) {
+  plugin_state->x = 50;
+  plugin_state->y = 30;
 }
 
 int32_t hello_world_app() {
-    FuriMessageQueue* event_queue = furi_message_queue_alloc(8, sizeof(PluginEvent));
+  FuriMessageQueue *event_queue =
+      furi_message_queue_alloc(8, sizeof(PluginEvent));
 
-    PluginState* plugin_state = malloc(sizeof(PluginState));
+  PluginState *plugin_state = malloc(sizeof(PluginState));
 
-    hello_world_state_init(plugin_state);
+  hello_world_state_init(plugin_state);
 
-    ValueMutex state_mutex;
-    if(!init_mutex(&state_mutex, plugin_state, sizeof(PluginState))) {
-        FURI_LOG_E("Hello_world", "cannot create mutex\r\n");
-        free(plugin_state);
-        return 255;
-    }
+  ValueMutex state_mutex;
+  if (!init_mutex(&state_mutex, plugin_state, sizeof(PluginState))) {
+    FURI_LOG_E("Hello_world", "cannot create mutex\r\n");
+    free(plugin_state);
+    return 255;
+  }
 
-    // Set system callbacks
-    ViewPort* view_port = view_port_alloc();
-    view_port_draw_callback_set(view_port, render_callback, &state_mutex);
-    view_port_input_callback_set(view_port, input_callback, event_queue);
+  // Set system callbacks
+  ViewPort *view_port = view_port_alloc();
+  view_port_draw_callback_set(view_port, render_callback, &state_mutex);
+  view_port_input_callback_set(view_port, input_callback, event_queue);
 
-    // Open GUI and register view_port
-    Gui* gui = furi_record_open("gui");
-    gui_add_view_port(gui, view_port, GuiLayerFullscreen);
+  // Open GUI and register view_port
+  Gui *gui = furi_record_open("gui");
+  gui_add_view_port(gui, view_port, GuiLayerFullscreen);
 
-    PluginEvent event;
-    for(bool processing = true; processing;) {
-        FuriStatus event_status = furi_message_queue_get(event_queue, &event, 100);
+  PluginEvent event;
+  for (bool processing = true; processing;) {
+    FuriStatus event_status = furi_message_queue_get(event_queue, &event, 100);
 
-        PluginState* plugin_state = (PluginState*)acquire_mutex_block(&state_mutex);
+    PluginState *plugin_state =
+        (PluginState *)acquire_mutex_block(&state_mutex);
 
-        if(event_status == FuriStatusOk) {
-            // press events
-            if(event.type == EventTypeKey) {
-                if(event.input.type == InputTypePress) {
-                    switch(event.input.key) {
-                    case InputKeyUp:
-                        plugin_state->y--;
-                        break;
-                    case InputKeyDown:
-                        plugin_state->y++;
-                        break;
-                    case InputKeyRight:
-                        plugin_state->x++;
-                        break;
-                    case InputKeyLeft:
-                        plugin_state->x--;
-                        break;
-                    case InputKeyOk:
-                    case InputKeyBack:
-                        processing = false;
-                        break;
-                    default:
-                        break;
-                    }
-                }
-            }
-        } else {
-            FURI_LOG_D("Hello_world", "FuriMessageQueue: event timeout");
-            // event timeout
+    if (event_status == FuriStatusOk) {
+      // press events
+      if (event.type == EventTypeKey) {
+        if (event.input.type == InputTypePress) {
+          switch (event.input.key) {
+          case InputKeyUp:
+            plugin_state->y--;
+            break;
+          case InputKeyDown:
+            plugin_state->y++;
+            break;
+          case InputKeyRight:
+            plugin_state->x++;
+            break;
+          case InputKeyLeft:
+            plugin_state->x--;
+            break;
+          case InputKeyOk:
+          case InputKeyBack:
+            processing = false;
+            break;
+          default:
+            break;
+          }
         }
-
-        view_port_update(view_port);
-        release_mutex(&state_mutex, plugin_state);
+      }
+    } else {
+      FURI_LOG_D("Hello_world", "FuriMessageQueue: event timeout");
+      // event timeout
     }
 
-    view_port_enabled_set(view_port, false);
-    gui_remove_view_port(gui, view_port);
-    furi_record_close("gui");
-    view_port_free(view_port);
-    furi_message_queue_free(event_queue);
-    delete_mutex(&state_mutex);
+    view_port_update(view_port);
+    release_mutex(&state_mutex, plugin_state);
+  }
 
-    return 0;
+  view_port_enabled_set(view_port, false);
+  gui_remove_view_port(gui, view_port);
+  furi_record_close("gui");
+  view_port_free(view_port);
+  furi_message_queue_free(event_queue);
+  delete_mutex(&state_mutex);
+
+  return 0;
 }
